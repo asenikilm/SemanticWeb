9b1d42a8620198aca33ef75df6f465f2ac4bb65c
asenikilm <iness10996@gmail.com>
2021-05-26 16:39:00 +0300

    Revert "Revert "add RDF reading and writing""
    This reverts commit 348ddf00e6cf8e04efb765a8b8d154d6d712fe0c.

---
+++ src/main/java/com/mycompany/semanticweb_1/jena/RDF.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.semanticweb_1.jena;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+
+/**
+ *
+ * @author Gradoboev
+ */
+public class RDF {
+    
+    public static final Model model = ModelFactory.createDefaultModel();
+    
+}
